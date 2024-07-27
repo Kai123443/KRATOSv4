73abb04f0a18638810720596ac07de97a2de51f7
Kai123443 <k4972817@gmail.com>
2024-07-27 12:38:37 +0530

    Update config.py

--- config.py
+++ config.py
@@ -2,7 +2,7 @@
 #SCRIPT BY VENOMxCRAZY
   
 #Enter Your Bot Token here get it from @botfarher
-BOT_TOKEN=("7068548626:AAFFOrPqvKneGjl74zErm-IiQbYdUdu5nqw")
+BOT_TOKEN=("7068548626:AAGl1duTWX-tSiD3b5CTIOltal3RFcMRz6Q")
 
   #Enter Your telegram username here without @
 OWNER_USERNAME=("MrKratos77")
